47c509a13b3464af83625ffd7a24ae4c104ac412
Alexander Tkachenko <sasha@markupus.tech>
2023-03-27 18:00:59 +0200

    feat: organize working environment with api open ai

---
+++ api/APIGetAITitles.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * API Register Route class
+ *
+ * @package MultiPOS - Point of Sale for WooCommerce
+ * @version 1.0.0
+ */
+
+namespace MOpenAi\api;
+
+
+defined('ABSPATH') || exit();
+
+
+    /**
+     * Register API Routes Class.
+     */
+final class APIGetAITitles extends AbstractAPIConnectOpenAi
+{
+
+
+    public function request($content)
+    {
+        $args = [
+            [
+                "role" => "system",
+                "content" => "Create 10 SEO titles for articles"
+            ],
+            [
+                "role" => "user",
+                "content" => $content
+            ]
+        ];
+        try {
+            return $this->createRequest($args)->toModel()->choices[0]->message->content;
+        } catch (ClientException $e) {
+            error_log('Can\'t create request for title');
+        }
+    }
+
+}
---
+++ api/AbstractAPIConnectOpenAi.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * API Register Route class
+ *
+ * @package MultiPOS - Point of Sale for WooCommerce
+ * @version 1.0.0
+ */
+
+namespace MOpenAi\api;
+use Symfony\Component\HttpClient\Psr18Client;
+use Tectalic\OpenAi\Authentication;
+use Tectalic\OpenAi\Client;
+use Tectalic\OpenAi\Manager;
+use Tectalic\OpenAi\Models\Completions\CreateRequest;
+
+defined('ABSPATH') || exit();
+
+
+    /**
+     * Register API Routes Class.
+     */
+abstract class AbstractAPIConnectOpenAi
+{
+    protected $openaiClient;
+    /**
+     * Configuration Variable
+     *
+     * @var array
+
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $options = get_option('option_name1');
+        $oai_key = $options['input'] ?? null;
+        if (!$oai_key) {
+            return;
+        }
+        $this->openaiClient = Manager::build(
+            new \GuzzleHttp\Client(),
+            new Authentication($oai_key)
+        );
+
+    }
+    protected function createRequest($args) {
+        return $this->openaiClient->chatCompletions()->create(
+            new \Tectalic\OpenAi\Models\ChatCompletions\CreateRequest([
+                'model' => 'gpt-3.5-turbo',
+                'messages' => $args,
+            ])
+        );
+    }
+    abstract protected function request($args);
+
+}
