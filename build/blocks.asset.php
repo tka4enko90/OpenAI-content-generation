e59eb8ea2d58abd7bfdf51cc2ad8eb11b889f8f0
Alexander Tkachenko <sasha@markupus.tech>
2023-04-10 15:43:52 +0200

    upd: auto save posts

--- build/blocks.asset.php
+++ build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-url'), 'version' => 'a75163a0d22aac1b4d45');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-url'), 'version' => '39a1943e689137f39d0f');
